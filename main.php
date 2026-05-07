7215dc08da80eab5019f68ef3cdf41639f168b2a
katyador57d-cmd <katyador57d@gmail.com>
2026-05-07 18:01:07 +0200

    feat: wire all classes in main entry point + new main.php

--- main.php
+++ main.php
@@ -7,43 +7,7 @@ require_once __DIR__ . '/src/MenuHandler.php';
 
 $display = new Display();
 
-/*$milk = null;
-$motherFucker = null;
-try {
-    $milk = new Product('Milk', 89.9, 1);
-    $motherFucker = new Product('Mother Fucker', 589.9, 2);
-    $display->printSuccess('Успешно добавлено');
-} catch (InvalidArgumentException $e) {
-    $display->printError($e->getMessage());
-}
-
-*/
 $cart = new Cart();
 
-//echo $milk->getQuantity() . "\n";
-
-/*$cart->addProduct($milk);
-echo $milk->getQuantity() . "\n";
-
-echo count($cart->getProducts()) . "\n";
-var_dump($cart->getDiscountAmount());
-var_dump($cart->getSubtotal());
-var_dump($cart->isDiscountApplied());
-var_dump($cart->getTotal());
-$display->printCart($cart);
-$display->printReceipt($cart);
-$display->printDiscointProgress($cart);
-
-try{
-    $cart->removeProduct($motherFucker);
-    $display->printSuccess("Успешно удалено");
-} catch (InvalidArgumentException $e) {
-    $display->printError($e->getMessage());
-}
-
-
-var_dump($cart->getSubtotal());
-
-*/
 $menu = new Menu($cart, $display);
 $menu->run();
