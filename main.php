82b251bed8e8b588d907874ebb2ce2112020cf39
katyador57d-cmd <katyador57d@gmail.com>
2026-05-07 17:50:30 +0200

    feat: add MenuHandler with user interaction loop

--- main.php
+++ main.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 require_once __DIR__ . '/src/Product.php';
 require_once __DIR__ . '/src/Cart.php';
 require_once __DIR__ . '/src/Display.php';
-require_once __DIR__ . '/src/MennuHandler.php';
+require_once __DIR__ . '/src/MenuHandler.php';
 
 $display = new Display();
 
